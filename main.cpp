84be5fcbefd6ffa2c1c6c7759cd91844229fc077
roberto <sonicultra565@gmail.com>
2025-10-04 18:27:57 +0200

    refactor: add class Object

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#ifndef M_PI
+#define M_PI 3.141592653589
+#endif
 
 float screenWidth = 800.0f;
 float screenHeight = 600.0f;
@@ -9,26 +12,65 @@ float screenHeight = 600.0f;
 GLFWwindow* StartGLFW();
 void DrawCircle(float centerX, float centerY, float radius, int res);
 
+class Object {
+    public:
+    std::vector<float> position;
+    std::vector<float> velocity;
+    float radius;
+    Object(std::vector<float> position, std::vector<float> velocity, float radius = 15.0f) {
+        this->position = position;
+        this->velocity = velocity;
+        this->radius = radius;
+    }
+
+    void accelerate(float x, float y) {
+        this->velocity[0] += x;
+        this->velocity[1] += y;
+    }
+
+    void updatePos() {
+        this->position[0] += this->velocity[0];
+        this->position[1] += this->velocity[1];
+    }
+
+    void DrawCircle(float centerX, float centerY, float radius, int res) {
+        glBegin(GL_TRIANGLE_FAN);
+        glVertex2d(centerX, centerY);
+        for (int i=0;i<=res;++i) {
+            float angle = 2.0f * M_PI * (static_cast<float>(i) / res);
+            float x = centerX + cos(angle) * radius;
+            float y = centerY + sin(angle) * radius;
+            glVertex2d(x, y);
+        }
+        glEnd();
+    }
+};
+
 int main() {
     GLFWwindow* window = StartGLFW();
     float centerX = screenWidth / 2.0f;
     float centerY = screenHeight / 2.0f;
     float radius = 50.0f;
     int res = 100;
+    std::vector<Object> objs = {
+        Object(std::vector<float>{200.0f, 500.0f}, std::vector<float>{5.0f, 0.0f}),
+        Object(std::vector<float>{700.0f, 500.0f}, std::vector<float>{5.0f, 0.0f})
+    };
     std::vector<float> position = {400.0f, 300.0f};
     std::vector<float> velocity = {0.0f, 0.0f};
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
-        DrawCircle(position[0], position[1], 50.0f, 50);
-        position[0] += velocity[0];
-        position[1] += velocity[1];
-        velocity[1] += -9.81f / 20.0f;
-        if (position[1] < 0 || position[1] > screenHeight) {
-            velocity[1] *= -0.95;
+        for (auto& obj : objs) {
+            obj.accelerate(0.0f, -9.81);
+            obj.updatePos();
+            obj.DrawCircle(obj.position[0], obj.position[1], obj.radius, res);
+            if (obj.position[1] < -obj.radius) {
+                obj.velocity[1] *= -0.95;
+            }
+            if (obj.position[0] < -obj.radius || (obj.position[0] + obj.radius) > screenWidth) {
+                obj.velocity[0] *= -0.95;
+            }
         }
-        if (position[0] < 0 || position[0] > screenWidth) {
-            velocity[0] *= -0.95;
-        } 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -47,16 +89,4 @@ GLFWwindow* StartGLFW() {
     glfwMakeContextCurrent(window);
     glOrtho(0, screenWidth, 0, screenHeight, -1, 1);
     return window;
-}
-
-void DrawCircle(float centerX, float centerY, float radius, int res) {
-    glBegin(GL_TRIANGLE_FAN);
-    glVertex2d(centerX, centerY);
-    for (int i=0;i<=res;++i) {
-        float angle = 2.0f * 3.141592653589 * (static_cast<float>(i) / res);
-        float x = centerX + cos(angle) * radius;
-        float y = centerY + sin(angle) * radius;
-        glVertex2d(x, y);
-    }
-    glEnd();
 }
