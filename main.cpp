c997392bf1ada66f4eca195839403031cac70d57
roberto <sonicultra565@gmail.com>
2025-10-04 16:26:49 +0200

    feat: add window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+GLFWwindow* StartGLFW();
+
 int main() {
+    GLFWwindow* window = StartGLFW();
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
     return 0;
+}
+
+GLFWwindow* StartGLFW() {
+    if (!glfwInit()) {
+        std::cerr << "failed to initialize glfw, panic!" << std::endl;
+        return nullptr;
+    }
+    GLFWwindow* window = glfwCreateWindow(800, 600, "gravity_sim", NULL, NULL);
+    return window;
 }
